f637001e05b5326614e98f5c1ddfa4c0daa7a266
Matthew Bunday <zenceph@gmail.com>
2014-12-09 10:44:33 -0500

    Add all and use view for single display

--- app/controllers/task.rb
+++ app/controllers/task.rb
@@ -1,6 +1,11 @@
 get '/task/:id' do |id|
   @task = Task.find(id)
-  @task.description
+  erb :'task/single', locals: {task: @task}
+end
+
+get '/tasks/all' do 
+  @tasks = Task.all
+  erb :'task/all'
 end
 
 get '/tasks/new' do
