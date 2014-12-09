31fb16a1173642f57c1e3b9927b3d5f1084d52c3
Matthew Bunday <zenceph@gmail.com>
2014-12-09 10:37:55 -0500

    Add POST routes.

--- app/controllers/task.rb
+++ app/controllers/task.rb
@@ -2,3 +2,13 @@ get '/task/:id' do |id|
   @task = Task.find(id)
   @task.description
 end
+
+get '/tasks/new' do
+  erb :'task/new'
+end
+
+post '/tasks' do
+  puts params.inspect
+  task = Task.create(params[:task])
+  redirect ("/task/#{task.id}")
+end
