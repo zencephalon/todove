2120ed92a7d3ddf1cdf69dfba00b8f9d24a2d043
Matthew Bunday <zenceph@gmail.com>
2014-12-10 14:47:47 -0500

    Add user authentication.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,9 +3,38 @@ get '/' do
 end
 
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    # tell the user they suck at logging in
+    redirect '/login'
+  end
 end
 
 get '/signup' do
-  
+  erb :'auth/signup' 
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    # display errors to user
+    redirect '/signup'
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
