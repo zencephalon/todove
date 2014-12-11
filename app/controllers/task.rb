06cc12ba81e578063a265c3e705568f4d5c687a0
Matthew Bunday <zenceph@gmail.com>
2014-12-11 14:19:07 -0500

    Protect routes from guest users.

--- app/controllers/task.rb
+++ app/controllers/task.rb
@@ -2,46 +2,44 @@ get '/' do
   redirect '/tasks/all'
 end
 
-get '/task/:id' do |id|
+get '/task/:id', auth: :user do |id|
   @task = Task.find(id)
   erb :'task/single', locals: {task: @task}
 end
 
-get '/tasks/all' do 
-  redirect '/login' unless current_user
-
+get '/tasks/all', auth: :user do 
   @tasks = current_user.tasks.sort_by {|t| t.priority}
   erb :'task/all'
 end
 
-get '/tasks/new' do
+get '/tasks/new', auth: :user do
   erb :'task/new'
 end
 
-post '/tasks' do
+post '/tasks', auth: :user do
   params[:task][:user_id] = current_user.id
   task = Task.create(params[:task])
   redirect ("/")
 end
 
-get '/task/:id/edit' do |id|
+get '/task/:id/edit', auth: :user do |id|
   @task = Task.find(id)
   erb :'task/edit'
 end
 
-put '/task/:id' do |id|
+put '/task/:id', auth: :user do |id|
   task = Task.find(id)
   task.update(params[:task])
   redirect ("/task/#{task.id}")
 end
 
-put '/task/:id/toggle_complete' do |id|
+put '/task/:id/toggle_complete', auth: :user do |id|
   task = Task.find(id)
   task.toggle_complete!
   redirect ("/")
 end
 
-delete '/task/:id' do |id|
+delete '/task/:id', auth: :user do |id|
   task = Task.find(id)
   task.destroy
   redirect "/tasks/all"
