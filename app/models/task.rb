efcf8548f7f45dab7be8e2a971d129e5e9d40ed7
Matthew Bunday <zenceph@gmail.com>
2014-12-12 10:26:01 -0500

    Add associations for subtasks.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,9 @@ class Task < ActiveRecord::Base
   has_many :task_tags
   has_many :tags, through: :task_tags
 
+  belongs_to :super_task, foreign_key: :parent_id, class_name: "Task"
+  has_many :sub_tasks, foreign_key: :parent_id, class_name: "Task", table_name: :tasks
+
   def toggle_complete!
     self.completed = !self.completed
     self.save
