adab1679c481746ecdfe99bb3fdc36931cb68dc3
Matthew Bunday <zenceph@gmail.com>
2014-12-15 14:57:55 -0500

    AJAX demo.

--- app/controllers/demo.rb
+++ app/controllers/demo.rb
@@ -6,3 +6,15 @@ end
 get '/get_cookie' do
   session[:religion]
 end
+
+get '/ajax_demo' do
+  erb :ajax_demo
+end
+
+post '/ajax_demo' do
+  p params
+  #sleep 3
+  #{}"Hello, #{params[:name]} from AJAX demo."
+  #erb :welcome
+  200
+end
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,29 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // $('#ajax_button').on('click', function(event) {
+
+  // })
+  $('#ajax_button').click(function(event) {
+    event.preventDefault();
+    console.log("Click received");
+
+    console.log("Before AJAX call.");
+    $.ajax({
+      type: "POST",
+      url: "/ajax_demo",
+      data: { name: "John", location: "Boston" }
+    }).done(function(response) {
+      console.log(response);
+    }).fail(function(response) {
+      console.log(response);
+    })
+
+    setTimeout(function() {console.log("After AJAX call.")}, 4000);
+  });
+
+  $('#demo_link').click(function(event) {
+    event.preventDefault();
+  })
+  //$('')
 });
